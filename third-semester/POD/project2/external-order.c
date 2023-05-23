5e04a3d228dfb1798e40a93dd5fcac4f40863584
Breno Soares <brenosoares4303@gmail.com>
2023-05-23 16:21:53 -0300

    project-2 POD continued

--- third-semester/POD/project2/external-order.c
+++ third-semester/POD/project2/external-order.c
@@ -1,33 +1,60 @@
+/**
+ * @file external-order.c
+ * @author Breno Soares Alves
+ * @version
+ * @date
+ * 
+ * @copyright
+ * 
+ * @brief
+ * 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MEMORY_LIMIT 3
 
-void openFile(FILE** file, char* filename, char* mode);
-void closeFile(FILE* file);
+// Auxiliary Functions
+
+void externalOrder(int *sortedVec, int *originalVec, int vecLen);
+void printVec(int *vec, int vecLen);
+
+// File Functions
+
+void openFile(FILE **file, char *filename, char *mode);
+void closeFile(FILE *file);
 
 int main()
 {
     int vec[] = {30, 21, 43, 3, 9, 82, 15};
-    FILE* file;
+    int vecLen = sizeof(vec) / sizeof(int);
+    int sortedVec[vecLen];
 
-    for(int i = 0; i < MEMORY_LIMIT; i++)
-    {
-        char str[100];
-        sprintf(str, "%s%d%s", "input", i, ".txt");
-        openFile(&file, str, "wt");
-    }
+    externalOrder(&sortedVec, vec, vecLen);
+
+    printVec(sortedVec, vecLen);
 
     return 0;
 }
 
-void openFile(FILE** file, char* filename, char* mode)
+void externalOrder(int *sortedVec, int *originalVec, int vecLen);
+
+
+void openFile(FILE **file, char *filename, char *mode)
 {
     *file = fopen(filename, mode);
-    if(*file == NULL)
+    if (*file == NULL)
     {
         printf("Can't open file\n");
         exit(1);
     }
+}
+
+void printVec(int *vec, int vecLen)
+{
+    for(int i = 0; i < vecLen; i++)
+        printf("%d ", vec[i]);
+    printf("\n");
 }
