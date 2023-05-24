1a97ddcd651aac67480fb48a58d46e2d334ed822
Breno Soares <brenosoares4303@gmail.com>
2023-05-24 15:53:20 -0300

    project2 POD continued

--- third-semester/POD/project2/README.md
+++ third-semester/POD/project2/README.md
@@ -4,6 +4,34 @@ inp1: 21 30 43
 inp2: 3 9 82
 inp3: 15
 
+inp1: 21 30 43
+inp2: 9 82
+inp3: 15
+
+inp1: 21 30 43
+inp2: 82
+inp3: 15
+
+inp1: 21 30 43
+inp2: 82
+inp3: 
+
+inp1: 30 43
+inp2: 82
+inp3: 
+
+inp1: 43
+inp2: 82
+inp3: 
+
+inp1:
+inp2: 82
+inp3: 
+
+inp1:
+inp2:
+inp3:
+
 out1: 3 9 15 21 30 43 82
 
 3 9 15 21 30 43 82
--- third-semester/POD/project2/external-order.c
+++ third-semester/POD/project2/external-order.c
@@ -3,18 +3,18 @@
  * @author Breno Soares Alves
  * @version
  * @date
- * 
+ *
  * @copyright
- * 
+ *
  * @brief
- * 
-*/
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MEMORY_LIMIT 3
+#define MEMORY_LIMIT 5
 
 // Auxiliary Functions
 
@@ -23,38 +23,55 @@ void printVec(int *vec, int vecLen);
 
 // File Functions
 
-void openFile(FILE **file, char *filename, char *mode);
+FILE** createFile(char *type);
 void closeFile(FILE *file);
 
 int main()
 {
     int vec[] = {30, 21, 43, 3, 9, 82, 15};
     int vecLen = sizeof(vec) / sizeof(int);
+
     int sortedVec[vecLen];
 
-    externalOrder(&sortedVec, vec, vecLen);
-
-    printVec(sortedVec, vecLen);
+    externalOrder(sortedVec, vec, vecLen);
 
     return 0;
 }
 
-void externalOrder(int *sortedVec, int *originalVec, int vecLen);
-
-
-void openFile(FILE **file, char *filename, char *mode)
+void externalOrder(int *sortedVec, int *originalVec, int vecLen)
 {
-    *file = fopen(filename, mode);
-    if (*file == NULL)
+    FILE **inputFiles;
+    // create inputs files
+    inputFiles = createFile("input");
+    // distribute values in input files ordenered
+
+    for(int i = 0; i < MEMORY_LIMIT; i++)
     {
-        printf("Can't open file\n");
-        exit(1);
+        printf("%c\n",fgetc(inputFiles[i]));
     }
+
+
+
+    // create output files
+    // distribute values in output files comparing first values in input files
+    // read output files
 }
 
-void printVec(int *vec, int vecLen)
+FILE** createFile(char *type)
 {
-    for(int i = 0; i < vecLen; i++)
-        printf("%d ", vec[i]);
-    printf("\n");
+    FILE** files = malloc(MEMORY_LIMIT * sizeof(FILE*));
+    for (int i = 0; i < MEMORY_LIMIT; i++)
+    {
+        char directory[20];
+        sprintf(directory, "./bin/%ss/%s%d%s", type, type, i, ".txt");
+        files[i] = fopen(directory, "r+");
+    }
+    return files;
 }
+
+// printf("File number: %d\n", i);
+    //     for (int j = 0; j < MEMORY_LIMIT && k < 7; j++)
+    //     {
+    //         printf("%d ", originalVec[k++]);
+    //     }
+    //     printf("\n");
