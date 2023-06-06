3329043879b6c8290cfc728ebdbd1f049bad146d
Breno Soares <brenosoares4303@gmail.com>
2023-06-06 15:37:10 -0300

    project2 - reaching...

--- third-semester/POD/project2/external-order.c
+++ third-semester/POD/project2/external-order.c
@@ -13,53 +13,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MEMORY_LIMIT 5
 
+#if defined(_WIN32)
+#define PLATFORM "windows"
+#elif defined(__unix__)
+#define PLATFORM "linux"
+#endif
+
+void externalSort(int* vec, int vecLen, int tradeFlag);
 FILE **openFiles(char *type);
 void closeFiles(FILE **fileArray);
 void sort(int *arr);
 void split(int *arr, FILE **files, int *vecIndex, int vecLen);
-void merge();
+int lowest(int *arr);
+int hasNumber(int *arr);
 
 int main()
 {
     int vec[] = {30, 21, 43, 3, 9, 82, 15, 23, -2, 0, -96, 4, 50, -9, 22, 99, 1002, 10, 40, 76, 77, 70, -70, -87, -90, -99, 21, -1, 1};
     int vecLen = sizeof(vec) / sizeof(int);
 
-    FILE **inputFiles = openFiles("input");
-    FILE **outputFiles = openFiles("output");
+    int tradeNumber = ((int)ceil(sqrt(vecLen)))%MEMORY_LIMIT;
 
-    int j = 0;
-    int outputCount = 0;
-    while (j < vecLen)
+    int trades = 0;
+    do
     {
-        split(vec, inputFiles, &j, vecLen);
-        outputCount++;
-    }
+        externalSort(vec, vecLen, trades);
 
-    // merge();
-    for (int k = 0; k < outputCount; k++)
-    {
-        int matrix[MEMORY_LIMIT][MEMORY_LIMIT];
-        for (int i = 0; i < MEMORY_LIMIT; i++)
-        {
-            if (k == 0)
-                rewind(inputFiles[i]);
+        invertFiles();
 
-            for (int j = 0; j < MEMORY_LIMIT; j++)
-            {
-                int n = __INT32_MAX__;
-                fscanf(inputFiles[i], "%d,", &n);
-                matrix[i][j] = n;
-            }
+        trades++;
+    } while (trades < tradeNumber);
+    
 
-            fseek(inputFiles[i], 1, SEEK_CUR);
-        }
-        printf("\n");
-    }
-
-    closeFiles(inputFiles);
 
     return 0;
 }
@@ -70,7 +59,12 @@ FILE **openFiles(char *type)
     for (int i = 0; i < MEMORY_LIMIT; i++)
     {
         char directory[30];
-        sprintf(directory, ".\\bin\\%ss\\%s%d%s", type, type, i, ".txt");
+
+        if (strcmp(PLATFORM, "linux") == 0)
+            sprintf(directory, "./bin/%ss/%s%d%s", type, type, i, ".txt");
+        else
+            sprintf(directory, ".\\bin\\%ss\\%s%d%s", type, type, i, ".txt");
+
         files[i] = fopen(directory, "w+");
         if (files[i] == NULL)
         {
@@ -136,15 +130,80 @@ void split(int *arr, FILE **files, int *vecIndex, int vecLen)
 
             if (i == arrLen - 1)
             {
-                fprintf(files[nFile], "%d ", nArray[i]);
+                fprintf(files[nFile], ",%d, ", nArray[i]);
                 continue;
             }
             else
-                fprintf(files[nFile], "%d,", nArray[i]);
+                fprintf(files[nFile], ",%d", nArray[i]);
         }
     }
 }
 
-void merge()
+int lowest(int *arr)
 {
+    int lower = 0;
+    for (int i = 1; i < MEMORY_LIMIT; i++)
+    {
+        if (arr[i] < arr[lower])
+            lower = i;
+    }
+
+    return lower;
+}
+
+int hasNumber(int *arr)
+{
+    int flag = 0;
+    for(int i = 0; i < MEMORY_LIMIT; i++)
+    {
+        if(arr[i] != __INT32_MAX__)
+            return 1;
+    }
+    return flag;
+}
+
+void externalSort(int* vec, int vecLen, int tradeFlag)
+{
+    FILE **inputFiles = openFiles("input");
+    FILE **outputFiles = openFiles("output");
+
+
+    int j = 0;
+    int outputCount = 0;
+    while (j < vecLen)
+    {
+        split(vec, inputFiles, &j, vecLen);
+        outputCount++;
+    }
+
+    for (int k = 0; k < outputCount; k++)
+    {
+        int arr[MEMORY_LIMIT];
+        for (int i = 0; i < MEMORY_LIMIT; i++)
+        {
+            if (k == 0)
+                fseek(inputFiles[i], 1, SEEK_SET);
+
+            fscanf(inputFiles[i], "%d,", &arr[i]);
+        }
+        while (hasNumber(arr) == 1)
+        {
+            int lowestIndex = lowest(arr);
+            fprintf(outputFiles[k], ",%d", arr[lowestIndex]);
+            int n;
+            arr[lowestIndex] = (fscanf(inputFiles[lowestIndex], "%d,", &n) == 1) ? n : __INT32_MAX__;
+        }
+
+        for(int i = 0; i < MEMORY_LIMIT; i++)
+        {
+            fseek(inputFiles[i], 1, SEEK_CUR);
+        }
+    }
+
+    for(int i = 0; i < outputCount; i++)
+    {
+        fprintf(outputFiles[i], ",");
+    }
+
+    closeFiles(inputFiles);
 }
